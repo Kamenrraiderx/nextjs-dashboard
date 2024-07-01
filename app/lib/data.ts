0f74873166cfe15660c9fd5e6e3ff8d591a4ac53
Anibal Reyes <areyes@redabierta.hn>
2024-07-01 13:56:41 -0600

    Dashboard feature

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -28,7 +28,10 @@ export async function fetchRevenue() {
   }
 }
 
+
+
 export async function fetchLatestInvoices() {
+
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
